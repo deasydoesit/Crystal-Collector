91f495874548652f43f4cb1de6a6b0b1eb007296
deasydoesit <darwin.rinderer@gmail.com>
2018-05-19 20:32:56 -0400

    Creating function to generate numbers assocaited with crystals 1-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,35 @@ var userLosses = 0;
 var score = 0;
 var randomNumber = 0;
 
+var crystOne = 0;
+var crystTwo = 0;
+var crystThree = 0;
+var crystFour = 0;
+
 var randomNumberDisplay = $("#random");
 var userWinsDisplay = $("#wins");
 var userLossesDisplay = $("#losses");
 var scoreDisplay = $("#score");
 
 function initialize(){
-    var randomNumber = Math.floor(Math.random() * 100);
+    randomNumber = Math.floor(Math.random() * 100);
     $(randomNumberDisplay).text(randomNumber);
     $(userLossesDisplay).text(0);
     $(userWinsDisplay).text(0);
     $(scoreDisplay).text(0);
 }
 
+function generateCrystalNums(num) {
+    crystOne = Math.ceil(num / (Math.random() * 10));
+    crystTwo = Math.ceil(num / (Math.random() * 10));
+    crystThree = Math.ceil(num / (Math.random() * 10));
+    crystFour = Math.ceil(num / (Math.random() * 10));
+}
+
 $(document).ready(function() {
 
-initialize();
+    initialize();
+    generateCrystalNums(randomNumber);
 
 
 });
